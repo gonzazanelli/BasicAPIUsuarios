d8c8ef445bf1d244fa773ab2208a09c0de04a9cd
gonzazanelli <GZ257768@fi365.ort.edu.uy>
2026-01-08 13:00:52 -0300

    crear un nuevo rol

--- src/routes/rol.js
+++ src/routes/rol.js
@@ -53,4 +53,52 @@ router.get('/usuarios/:identificador', async (req, res) => {
   } 
 });
 
+router.post('/crear', async (req, res) => {
+  try {
+    const pool = await poolPromise;
+
+    if (!pool) {
+      return res.status(500).json({ error: 'No hay conexión a la base de datos' });
+    }
+
+    const { nombre } = req.body;
+
+    if (!nombre || nombre.trim() === '') {
+      return res.status(400).json({ error: 'El nombre del rol es obligatorio' });
+    }
+
+    const nombreMayus = nombre.trim().toUpperCase();
+
+    // Control de duplicados
+    const existeRol = await pool
+      .request()
+      .input('nombre', sql.VarChar, nombreMayus)
+      .query(`
+        SELECT COUNT(*) AS cantidad
+        FROM Rol
+        WHERE nombre = @nombre
+      `);
+
+    if (existeRol.recordset[0].cantidad > 0) {
+      return res.status(409).json({ error: 'El rol ya existe' });
+    }
+
+    // Se realiza el alta
+    const result = await pool
+    .request()
+    .input('nombre', sql.VarChar, nombreMayus)
+    .query(`
+      INSERT INTO Rol (nombre)
+      OUTPUT INSERTED.identificador
+      VALUES (@nombre)
+    `);
+
+    res.status(201).json({ identificador: result.recordset[0].identificador });
+
+  } catch (err) {
+    console.error('Error al crear el rol:', err);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
 module.exports = router;
